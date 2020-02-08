6b314712081d26a708b5b8aa38f118f5f1421d2a
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-08 20:43:44 +0300

    Add project files.

---
+++ ArrayListApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
